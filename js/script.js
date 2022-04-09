8dfe499fcad62a99c6dc21b9ecc168a6e98f30f7
dmasdi123 <febriandimas634@gmail.com>
2022-04-09 16:03:48 +0700

    add fiture API from omdbapi

--- js/script.js
+++ js/script.js
@@ -1,53 +1,32 @@
-function tampilkanSemuaMenu() {
-  $.getJSON("data/pizza.json", function (data) {
-    let menu = data.menu;
-    $.each(menu, function (i, data) {
-      $("#daftar-menu").append(
-        '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' +
-          data.gambar +
-          '" class="card-img-top"><div class="card-body"><h5 class="card-title">' +
-          data.nama +
-          '</h5><p class="card-text">' +
-          data.deskripsi +
-          '</p><h5 class="card-title">Rp. ' +
-          data.harga +
-          '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>'
-      );
-    });
-  });
-}
-tampilkanSemuaMenu();
-
-$(".nav-link").on("click", function () {
-  $(".nav-link").removeClass("active");
-  $(this).addClass("active");
-
-  let kategori = $(this).html();
-  $("#kategori").html(kategori);
-
-  if (kategori == "All Menu") {
-    tampilkanSemuaMenu();
-    return;
-  }
-
-  $.getJSON("data/pizza.json", function (data) {
-    let menu = data.menu;
-    let content = "";
-
-    $.each(menu, function (i, data) {
-      if (data.kategori == kategori.toLowerCase()) {
-        content +=
-          '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' +
-          data.gambar +
-          '" class="card-img-top"><div class="card-body"><h5 class="card-title">' +
-          data.nama +
-          '</h5><p class="card-text">' +
-          data.deskripsi +
-          '</p><h5 class="card-title">Rp. ' +
-          data.harga +
-          '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
+$('#search-button').on('click', function () {
+  $.ajax({
+    url: 'http://omdbapi.com',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      'apikey' : '51559b83',
+      's' : $('#search-input').val()
+    },
+    success: function (result) {
+      if(result.Response == "True") {
+        let movies = result.Search;
+        $.each(movies, function(i, data) {
+          $('#movie-list').append(`
+          <div class="col-md-4">
+          <div class="card mb-4">
+          <img src="`+ data.Poster +`" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">`+ data.Title +`</h5>
+            <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+            <a href="#" class="btn btn-primary">See Detail</a>
+          </div>
+          </div>
+          </div>
+          `);
+        });
+      }else {
+        $('#movie-list').html(`<h1 class="text-center">` + result.Error + `</h1>`);
       }
-    });
-    $("#daftar-menu").html(content);
+    },
   });
 });
